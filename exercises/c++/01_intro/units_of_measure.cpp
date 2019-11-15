945e9f4fb74b1b1ec90cc9504252c01e0be70c11
FedericoPolli <polli.f@outlook.it>
2019-11-15 14:38:22 +0100

    w

--- exercises/c++/01_intro/units_of_measure.cpp
+++ exercises/c++/01_intro/units_of_measure.cpp
@@ -3,21 +3,26 @@
 #include <algorithm>
 
 int main () {
-  enum constants {inchtometre=0.0254}
-  std::cout << "Enter the number with its unit of measure" << std::endl;
-  std::string line="";
-  std::getline(std::cin, line);
-  std::string value(line.cbegin(), std::find(line.cbegin(), line.cend(), ' '));
-  std::string unit(std::find(line.cbegin(), line.cend(), ' ')+1, line.cend());
-  switch(unit) {
-  case 'inch':
-    std::cout << std::stof(value)*inchtometre << " m" << std::endl;
+  const double inchestometres=0.0254;
+  std::cout << "Accepted units of measure: inches, metres" << std::endl; 
+  while (true) {
+    std::cout << "Enter the number with its unit of measure" << std::endl;
+    std::string line="";
+    std::getline(std::cin, line);
+    std::string value(line.cbegin(), std::find(line.cbegin(), line.cend(), ' '));
+    std::string unit(std::find(line.cbegin(), line.cend(), ' ')+1, line.cend());
+    if (unit=="inches")
+      {
+      std::cout << std::stof(value)*inchestometres << " metres" << std::endl;
+      break;
+	}
+    if (unit=="metres"){
+      std::cout << std::stof(value) << " metres" << std::endl;
+      break;
+	}
+    else
+      std::cout << "Unknown unit of measure" << std::endl;
 
-  default:
-    std::cout << "Unknown unit of measure" << std::endl;
-
-
-
-
+  }
 
 }
